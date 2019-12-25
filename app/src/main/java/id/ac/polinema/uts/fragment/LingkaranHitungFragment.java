f17d5ca7dd3d14cc4c18f60622e5b3db0ed11906
Geraldo B. I <geraldoarema87@gmail.com>
2019-12-26 00:46:53 +0700

    Menyelesaikan Bangun2D

--- app/src/main/java/id/ac/polinema/uts/fragment/LingkaranHitungFragment.java
+++ app/src/main/java/id/ac/polinema/uts/fragment/LingkaranHitungFragment.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import id.ac.polinema.uts.R;
 
@@ -15,6 +18,10 @@ import id.ac.polinema.uts.R;
  * A simple {@link Fragment} subclass.
  */
 public class LingkaranHitungFragment extends Fragment {
+    private EditText inputanR, inputanR2;
+    private Button btnKeliling, btnLuas;
+
+    private TextView hasil, hasil2;
 
 
     public LingkaranHitungFragment() {
@@ -26,7 +33,33 @@ public class LingkaranHitungFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lingkaran_hitung, container, false);
+        View view = inflater.inflate(R.layout.fragment_lingkaran_hitung, container, false);
+
+        hasil = view.findViewById(R.id.fragmentSegitigaHitung_hasilKelilingText);
+        hasil2 = view.findViewById(R.id.outputLuas);
+        btnKeliling = view.findViewById(R.id.fragmentSegitigaHitung_hasilKeliling);
+        btnLuas = view.findViewById(R.id.hasilLuas);
+        inputanR = view.findViewById(R.id.fragmentSegitigaHitung_inputB);
+        inputanR2 = view.findViewById(R.id.editText4);
+
+        btnKeliling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(inputanR != null){
+                    hasil.setText(String.valueOf((Float.parseFloat(inputanR.getText().toString())) * 3.14 * 2));
+                }
+            }
+        });
+
+        btnLuas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(inputanR2 != null){
+                    hasil2.setText(String.valueOf((Float.parseFloat(inputanR2.getText().toString())) * 3.14 * (Float.parseFloat(inputanR2.getText().toString()))));
+                }
+            }
+        });
+        return view;
     }
 
 }
--- app/src/main/java/id/ac/polinema/uts/fragment/SegitigaHitungFragment.java
+++ app/src/main/java/id/ac/polinema/uts/fragment/SegitigaHitungFragment.java
@@ -24,10 +24,10 @@ import id.ac.polinema.uts.R;
  */
 public class SegitigaHitungFragment extends Fragment {
 
-    private EditText inputanA, inputanB, inputanC;
-    private Button btnKeliling;
+    private EditText inputanA, inputanB, inputanC, inputanD, inputanE;
+    private Button btnKeliling, btnLuas;
 
-    private TextView hasil;
+    private TextView hasil, hasil2;
 
     public SegitigaHitungFragment() {
         // Required empty public constructor
@@ -40,17 +40,32 @@ public class SegitigaHitungFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_segitiga_hitung, container, false);
         hasil = view.findViewById(R.id.fragmentSegitigaHitung_hasilKelilingText);
+        hasil2 = view.findViewById(R.id.outputLuas);
         btnKeliling = view.findViewById(R.id.fragmentSegitigaHitung_hasilKeliling);
+        btnLuas = view.findViewById(R.id.hasilLuas);
         inputanA = view.findViewById(R.id.fragmentSegitigaHitung_inputA);
         inputanB = view.findViewById(R.id.fragmentSegitigaHitung_inputB);
         inputanC = view.findViewById(R.id.fragmentSegitigaHitung_inputC);
+        inputanD = view.findViewById(R.id.editText4);
+        inputanE = view.findViewById(R.id.editText5);
+
 
         btnKeliling.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(inputanA.getText() != null || inputanB.getText() != null || inputanC.getText() != null){
 
-                    hasil.setText(String.valueOf(Float.parseFloat(inputanA.getText().toString())) + (Float.parseFloat(inputanB.getText().toString())) + (Float.parseFloat(inputanC.getText().toString())));
+                    hasil.setText(String.valueOf((Float.parseFloat(inputanA.getText().toString()) + Float.parseFloat(inputanB.getText().toString()) + Float.parseFloat(inputanC.getText().toString()))));
+                }
+            }
+        });
+
+        btnLuas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(inputanD.getText() != null || inputanE.getText() != null){
+
+                    hasil2.setText(String.valueOf((Float.parseFloat(inputanD.getText().toString()) * Float.parseFloat(inputanE.getText().toString()) /2)));
                 }
             }
         });
